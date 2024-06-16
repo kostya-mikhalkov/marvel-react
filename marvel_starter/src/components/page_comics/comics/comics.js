3ffea5f63fd9099c7a8eab6d362d57a92394ccb5
Konstantin <kostya.mikhalkov@bk.ru>
2024-06-16 21:14:30 +0300

    feat: added card

--- marvel_starter/src/components/page_comics/comics/comics.js
+++ marvel_starter/src/components/page_comics/comics/comics.js
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import { ComicsHeader } from "../comics_header/ComicsHeader";
 import { ComicsServices } from "../../../service/ComicsServices";
+import './comics.css';
 
 export const Comics = () => {
     const [char, setChar] = useState([]);
@@ -31,7 +32,7 @@ const onCharLoaded = (newChar) => {
 }
 
 const onRenderListElemnt = (char) => {
-    const elem = char.map(({id, prices, images, description, bool}, ind) => {
+    const elem = char.map(({id, prices, images, description, title, bool}, ind) => {
         let charImgStyle = bool ? 'contain' : 'cover';
         return (
             <li className="char__item"
@@ -39,21 +40,28 @@ const onRenderListElemnt = (char) => {
                 tabIndex={0}
                 >
                 <img src={images} style={{objectFit: `${charImgStyle}`}} alt="abyss"/>
-                <div className="char__name">{description}</div>
+                <div className="char__name comics__item">{title}</div>
+                <span>{prices}</span>
             </li>
         )
     });
     return (
-        <ul className="char__grid">
+        <ul className="comics__list">
             {elem}
         </ul>
     )
 }
-
+const elem = onRenderListElemnt(char);
     return (
         <div>
             <ComicsHeader />
-            {onRenderListElemnt(char)}
+            {elem}
+                <button className="button button__main button__long"
+                        disabled={newItemLoaded}
+                        style={{'display': charEnded ? 'none' : 'block'}}
+                        onClick={() => onRequest(offset)}>
+                    <div className="inner">load more</div>
+                </button>
         </div>
     )
 }
