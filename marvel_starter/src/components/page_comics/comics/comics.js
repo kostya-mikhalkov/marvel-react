9db327876931728a12022d87cc10a544cf5a8c59
Konstantin <kostya.mikhalkov@bk.ru>
2024-06-16 20:03:16 +0300

    feat: added comics list

--- marvel_starter/src/components/page_comics/comics/comics.js
+++ marvel_starter/src/components/page_comics/comics/comics.js
@@ -3,15 +3,57 @@ import { ComicsHeader } from "../comics_header/ComicsHeader";
 import { ComicsServices } from "../../../service/ComicsServices";
 
 export const Comics = () => {
-const [state, setState] = useState(null);
+    const [char, setChar] = useState([]);
+const [newItemLoaded, setNewItemLoaded] = useState(false);
+const [offset, setOffset] = useState(210);
+const [charEnded, setCharEnded] = useState(false);
 const {loading, error, getAllCharacters, getCharacters, clearError} = ComicsServices();
+
 useEffect(() => {
-    // setState(getAllCharacters());
-}, [])
-// console.log(state)
+    onRequest();
+}, []);
+
+const onRequest = (offset, initial) => {
+    initial ? setNewItemLoaded(false) : setNewItemLoaded(true);
+        getAllCharacters(offset)
+        .then(onCharLoaded)
+}
+
+const onCharLoaded = (newChar) => {
+    let ended = false;
+    if (newChar < 8) {
+        ended = true;
+    }
+    setChar(char => [...char, ...newChar]);
+    setNewItemLoaded(false);
+    setOffset(offset => offset + 8);
+    setCharEnded(ended);
+}
+
+const onRenderListElemnt = (char) => {
+    const elem = char.map(({id, prices, images, description, bool}, ind) => {
+        let charImgStyle = bool ? 'contain' : 'cover';
+        return (
+            <li className="char__item"
+                key={id}
+                tabIndex={0}
+                >
+                <img src={images} style={{objectFit: `${charImgStyle}`}} alt="abyss"/>
+                <div className="char__name">{description}</div>
+            </li>
+        )
+    });
+    return (
+        <ul className="char__grid">
+            {elem}
+        </ul>
+    )
+}
+
     return (
         <div>
             <ComicsHeader />
+            {onRenderListElemnt(char)}
         </div>
     )
 }
